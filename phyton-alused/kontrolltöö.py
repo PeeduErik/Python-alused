8a839c64d86eb37dafd407210939c3d3e549817e
PeeduErik <98737136+PeeduErik@users.noreply.github.com>
2022-03-16 20:50:12 +0200

    Update kontrolltöö.py

--- phyton-alused/kontrolltöö.py
+++ phyton-alused/kontrolltöö.py
@@ -9,8 +9,6 @@ keskmine = 0
 keskmine1 = 0
 suur = 0
 suur1 = 0
-raham=[2340,2570,1960,2900,2850,2570]
-rahan=[2060,2250,2370,2340,2120]
 
 #faili avamine
 with open("palk.txt", "r")as fail:
@@ -20,11 +18,11 @@ with open("palk.txt", "r")as fail:
         if rida[2] == "m":
             summa += int(rida[3])
             keskmine = summa/6
-            
+            maks = max(rida[3])
         elif rida[2] == "n":
             summa1 += int(rida[3])
             keskmine1 = summa1/len(rida[3])
-           
+            maks1 = max(rida[3])
             
         
 #Keskmiste välja andmine        
@@ -36,8 +34,9 @@ vahe=keskmine-keskmine1
 print("Meeste palk on ",vahe,"€ võrra suurem.")
 
 #Kellel on max palk
-print ("Naiste suurim palk on: ",max(rahan),"€.")
-print ("Meeste suurim palk on: ",max(raham),"€.")
+
+print ("Naiste suurim palk on: ",maks,"€.")
+print ("Meeste suurim palk on: ",maks1,"€.")
 
 #Keskmiste palkade võrdlemine
 if keskmine1 < keskmine:
